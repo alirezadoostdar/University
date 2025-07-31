b7e5d4dca0b0af7c006e1cb33c9725e9d12897c4
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-31 23:10:18 +0200

    add student service

--- src/Core/University.Application/Students/StudentService.cs
+++ src/Core/University.Application/Students/StudentService.cs
@@ -1,12 +1,39 @@
 ﻿using University.Application.Students.Contracts.Dtos;
+using University.Domain.Entities;
+using University.Domain.Interfaces;
 
 namespace University.Application.Students;
 
 public class StudentService : IStudentService
 {
+    private readonly IStudentRepository _studentRepository;
+
+    public StudentService(IStudentRepository studentRepository)
+    {
+        _studentRepository = studentRepository;
+    }
+
     public int Add(AddStudentDto dto)
     {
-        throw new NotImplementedException();
+        var student = new Student
+        {
+            Name = dto.Name,
+            Family = dto.Family,
+            BirthDate = dto.BirthDate,
+            PhoneNumber = dto.PhoneNumber,
+            Address = dto.Address,
+            IdentityCode = dto.IdentityCode,
+            StudentCode = GenerateStudentCode()
+        };
+
+        _studentRepository.Add(student);
+        return student.Id;
+    }
+
+    private int GenerateStudentCode()
+    {
+        string timePart = DateTime.Now.ToString("yyyyMMddHHmmss");
+        return int.Parse(timePart.Substring(2));
     }
 
     public void Delete(int id)
